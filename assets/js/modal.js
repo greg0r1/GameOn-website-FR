9a83d12d210950a4b3abebe1802ade9f73af7e6f
GREGORY DERNAUCOURT <gregorydernaucourt@mbp-de-gregory.home>
2021-07-01 14:42:59 +0200

    javascript retiré du html

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -1,7 +1,12 @@
-function editNav() {
+// Edit nav resonsive
+const responsiveNavMenu = document.getElementById('responsiveNavMenu');
+responsiveNavMenu.addEventListener('click', editNav);
+
+function editNav(e) {
   var x = document.getElementById("myTopnav");
   if (x.className === "topnav") {
     x.className += " responsive";
+    e.preventDefault();
   } else {
     x.className = "topnav";
   }
@@ -11,13 +16,20 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const modalClose = document.querySelector(".bground .close");
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
-}
+ modalbg.style.display = "block";
+} 
 
+// close modal event
+modalClose.addEventListener("click", closeModal);
 
+// Close modal form
+function closeModal() {
+ modalbg.style.display = "none";
+} 
