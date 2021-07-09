d7edcc5d8a88ce5a4999367ed9393f39a9391794
GREGORY DERNAUCOURT <gregorydernaucourt@mbp-de-gregory.home>
2021-07-09 17:03:45 +0200

    refactorisation

--- assets/js/modal.js
+++ assets/js/modal.js
@@ -3,14 +3,14 @@ const responsiveNavMenu = document.getElementById('responsiveNavMenu');
 responsiveNavMenu.addEventListener('click', editNav);
 
 function editNav(e) {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-    e.preventDefault();
-  } else {
-    x.className = "topnav";
-    e.preventDefault();
-  }
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+        e.preventDefault();
+    } else {
+        x.className = "topnav";
+        e.preventDefault();
+    }
 }
 
 // DOM Elements
@@ -19,12 +19,24 @@ const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 const modalClose = document.querySelector(".bground .close");
 
+const inputFirstNameElt = document.getElementById("first");
+const inputLastNameElt = document.getElementById("last");
+const inputEmailElt = document.getElementById("email");
+const inputBirthdateElt = document.getElementById("birthdate");
+const inputQuantityElt = document.getElementById("quantity");
+const inputsTypeRadio = document.querySelectorAll("input[type='radio']");
+const inputTypeCheckbox = document.getElementById('checkbox1');
+const inputSubmitElt = document.querySelector(".btn-submit");
+const formElt = document.getElementsByTagName('form')[0];
+const error = document.querySelector('.error');
+const formDataList = document.getElementsByClassName('formData');
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+    modalbg.style.display = "block";
 }
 
 // close modal event
@@ -32,179 +44,108 @@ modalClose.addEventListener("click", closeModal);
 
 // Close modal form
 function closeModal() {
-  modalbg.style.display = "none";
+    modalbg.style.display = "none";
 }
 
 
 
-// Validation form elements
-const inputFirstNameElt = document.getElementById("first");
-const inputLastNameElt = document.getElementById("last");
-const inputEmailElt = document.getElementById("email");
-const inputBirthdateElt = document.getElementById("birthdate");
-const inputQuantityElt = document.getElementById("quantity");
-const formSubmitElt = document.querySelector("input[type='submit']");
+// Patterns des champs
+const regExName = "[a-zA-Z]{3,}$";
+inputFirstNameElt.pattern = regExName;
+inputLastNameElt.pattern = regExName;
+inputEmailElt.pattern = "[a-z0-9\-_]+[a-z0-9\.\-_]*@[a-z0-9\-_]{2,}\.[a-z\.\-_]+[a-z\-_]+";
+inputBirthdateElt.min = "1900-01-01"
+inputBirthdateElt.max = "2015-01-01"
+var inputCheckedElement = 0;
 
-
-// Validation des champs sur event
-
-// Message erreur activation class
-function errorMessage(stringMessage, strinColor) {
-  const newDiv = document.querySelector('.content').appendChild(document.createElement('div'));
-  newDiv.classList.add('errorMessage');
-  newDiv.style.display = 'flex';
-  newDiv.style.background = strinColor;
-  newDiv.innerHTML = '<p>' + stringMessage + '</p>';
-  setTimeout(() => newDiv.style.display = 'none', 3000);
-
-}
-
-// check prénom 
-inputFirstNameElt.addEventListener('change', verifFirst);
-
-function verifFirst(e) {
-  let rgexName = /[a-zA-Z]{3,}$/;
-  let valueFirst = inputFirstNameElt.value;
-  let checkRgexName = rgexName.test(valueFirst);
-
-  if (!checkRgexName) {
-    e.preventDefault();
-    errorMessage('Veuillez saisir un prénom valide', 'red')
-  } else {
-    return true
-  }
-}
-
-// check nom 
-inputLastNameElt.addEventListener('change', verifLast);
-
-function verifLast(e) {
-  let rgexName = /[a-zA-Z]{3,}$/;
-  let valueLast = inputLastNameElt.value;
-  let checkRgexName = rgexName.test(valueLast);
-
-  if (!checkRgexName) {
-    e.preventDefault();
-    errorMessage('Veuillez saisir un nom valide', 'red')
-  } else {
-    return true
-  }
-}
-
-// check email
-inputEmailElt.addEventListener('change', verifEmail)
-
-function verifEmail(e) {
-  let valueEmail = inputEmailElt.value;
-  let rgexMail = /[a-z0-9\-_]+[a-z0-9\.\-_]*@[a-z0-9\-_]{2,}\.[a-z\.\-_]+[a-z\-_]+/gmi;
-  let checkRgexEmail = rgexMail.test(valueEmail);
-
-  if (!checkRgexEmail) {
-    e.preventDefault();
-    errorMessage('Veuillez saisir un email valide', 'red')
-  } else {
-    return true
-  }
-}
-
-// check birthdate
-inputBirthdateElt.addEventListener('focusout', verifBirthdate);
-
-function verifBirthdate(e) {
-  let valueBirthdate = inputBirthdateElt.value;
-  let validDateMin = Date.parse(valueBirthdate);
-  let validDateMax = Date.parse('2022-01-01');
-
-
-  if (validDateMin < 0 || validDateMin > validDateMax) {
-    e.preventDefault();
-    errorMessage('Veuillez saisir une date d\'anniversaire valide', 'red')
-  } else {
-    return true
-  }
-}
-
-// check nombre de concours
-inputQuantityElt.addEventListener('focusout', verifQuantity)
-
-function verifQuantity(e) {
-  let valueQuantity = inputQuantityElt.value;
-
-  if (valueQuantity == '' || valueQuantity < 0 || valueQuantity > 50) {
-    e.preventDefault();
-    errorMessage('Veuillez saisir un nombre de tournois valide', 'red')
-  } else {
-    return true
-  }
-}
-
-// Validation all form
-formSubmitElt.addEventListener('click', submitValidation);
-
-function checkFields(e) {
-  verifFirst(e);
-  verifLast(e);
-  verifEmail(e);
-  verifBirthdate(e);
-  verifQuantity(e);
-}
-
-function submitValidation(e) {
-  let form = document.getElementById('validate');
-  var inputsOk = 0;
-  for (let i = 0; i < form.elements.length; i++) {
-    let input = form[i];
-
-    switch (input.type) {
-      case "text":
-        if (input.value == "") inputsOk++;
-        break;
-
-      case "email":
-        if (input.value == "") inputsOk++;
-        break;
-
-      case "date":
-        if (input.value == "") inputsOk++;
-        break;
-
-      case "number":
-        if (input.value == "") inputsOk++;
-        break;
-
-      case "radio":
-        let inputsRadio = document.querySelectorAll('input[type=radio]')
-        let count = 0;
-
-        for (let index = 0; index < inputsRadio.length; index++) {
-          const element = inputsRadio[index];
-          if (element.checked) {
-            count++
-          };
+// Fonction qui vérifie si un input radio est checked, 
+// si oui: inputCheckedElement passe à 1
+function verifyInputCheckedElement() {
+    for (let element of inputsTypeRadio) {
+        if (element.checked) {
+            inputCheckedElement++
         }
-
-        if (count == 0) {
-          inputsOk++
-        }
-        break;
-
-      default:
-        break;
     }
-  }
-  checkFields(e);
-  let validationInputs = checkFields(e) || true;
-
-  // Check conditions d'utilisation avant envoi du formulaire
-  let required = document.getElementById('checkbox1');
-  if (!required.checked) {
-    e.preventDefault();
-    errorMessage('Veuillez accepter les conditions d\'utilisation', 'red')
-  } else if (inputsOk > 0) {
-    e.preventDefault();
-    errorMessage('Veuillez remplir tous les champs du formulaire', 'red')
-  } else if (inputsOk === 0 && validationInputs) {
-    errorMessage('Formulaire envoyé', 'green');
-    setTimeout(e.preventDefault(), 3000);
-  }
 }
+
+// Fonction qui vérifie la validité du champ
+function checkField(inputElement, errorSelectElement) {
+    let errorElt = document.querySelector(errorSelectElement);
+    if (inputElement.validity.valid) {
+        errorElt.innerHTML = "";
+        inputElement.classList.remove('error-input');
+    } else {
+        inputElement.classList.add('error-input');
+    }
+}
+
+// Checks des inputs sur event
+inputFirstNameElt.addEventListener("input", () => checkField(inputFirstNameElt, '#first ~ .error'));
+
+inputLastNameElt.addEventListener("input", () => checkField(inputLastNameElt, '#last ~ .error'));
+
+inputEmailElt.addEventListener("input", () => checkField(inputEmailElt, '#email ~ .error'));
+
+inputBirthdateElt.addEventListener("input", () => checkField(inputBirthdateElt, '#birthdate ~ .error'));
+
+inputQuantityElt.addEventListener("input", () => checkField(inputQuantityElt, '#quantity ~ .error'));
+
+// Fonction qui vérifie les champs avant l'envoie du formulaire
+function verifyFieldsBeforeSubmit(event) {
+    // Chaque fois que l'utilisateur tente d'envoyer les données
+    // on vérifie que les champs soient valides.
+    verifyInputCheckedElement();
+    if (!inputFirstNameElt.validity.valid || inputFirstNameElt.value == "") {
+
+        // S'il est invalide, on affiche un message d'erreur personnalisé
+        document.querySelector('#first ~ .error').innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom.";
+
+        // Et on empêche l'envoi des données du formulaire
+        event.preventDefault();
+    } else if (!inputLastNameElt.validity.valid || inputLastNameElt.value == "") {
+
+        // S'il est invalide, on affiche un message d'erreur personnalisé
+        document.querySelector('#last ~ .error').innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom.";
+        // Et on empêche l'envoi des données du formulaire
+        event.preventDefault();
+    } else if (!inputEmailElt.validity.valid || inputEmailElt.value == "") {
+
+        document.querySelector('#email ~ .error').innerHTML = "Vous devez entrer un email valide.";
+        event.preventDefault();
+
+    } else if (!inputBirthdateElt.validity.valid || inputBirthdateElt.value == "") {
+
+        document.querySelector('#birthdate ~ .error').innerHTML = "Vous devez entrer votre date de naissance";
+        event.preventDefault();
+
+    } else if (!inputQuantityElt.validity.valid || inputQuantityElt == "") {
+
+        document.querySelector('#quantity ~ .error').innerHTML = "Vous devez entrer nombre de tournois valide";
+        event.preventDefault();
+
+    } else if (inputCheckedElement === 0) {
+
+        document.querySelector('#location6 ~ .error').innerHTML = "Vous devez choisir une option";
+        event.preventDefault();
+
+    } else if (!inputTypeCheckbox.checked) {
+
+        document.querySelector('#checkbox1 ~ .error').innerHTML = "Vous devez vérifier que vous acceptez les termes et conditions.";
+        event.preventDefault();
+
+
+    } else {
+        event.preventDefault();
+        let div = document.createElement('div');
+        div.classList.add('submittedMessage');
+        formElt.insertBefore(div, document.querySelector('.formData'));
+        div.innerHTML = "Merci d\'avoir soumis vos informations d\'inscription";
+        inputSubmitElt.value = "Fermer";
+        inputSubmitElt.classList.add('closeBtnSubmit');
+        inputSubmitElt.addEventListener('click', () => {
+            formElt.submit();
+        });
+    }
+}
+// Passagde de la fonction dans l'event submit button
+inputSubmitElt.addEventListener("click", (event) => verifyFieldsBeforeSubmit(event), false);
